4760b4211618706e65eccb34d5c3d014e22501cb
harsho-afk <harshofury@gmail.com>
2024-03-29 23:40:22 +0530

    remove test

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-from PIL import Image
-
-url = "https://www.reddit.com/r/memes/hot/.json"
-r = requests.get(url).json()
-print(requests.get(url,headers = {'User-agent': '1'}).json())
