44a5e70e8e4590f0a8eb4034cd947c5b4298b885
harsho-afk <harshofury@gmail.com>
2024-03-29 18:42:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import tweepy
+import tkinter
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+consumer_key = os.getenv('consumer_key')
+consumer_secret = os.getenv('consumer_secret')
+access_token = os.getenv('access_token')
+access_token_secret = os.getenv('access_token_secret')
+bearer_token=os.getenv('bearer_token')
+
+client = tweepy.Client(bearer_token=bearer_token,
+                       consumer_key=consumer_key,
+                       consumer_secret=consumer_secret,
+                       access_token=access_token,
+                       access_token_secret=access_token_secret,
+                       wait_on_rate_limit=True)
